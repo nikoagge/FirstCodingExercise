df6cc333e47c7599a969d3326155c84311a99c41
nikolas <nikoagge@gmail.com>
2019-10-31 17:39:26 +0200

    Created and auto layout UI complete programmatically.

--- FirstCodingExercise/Home/Controller/HomeController.swift
+++ FirstCodingExercise/Home/Controller/HomeController.swift
@@ -13,13 +13,38 @@ import UIKit
 class HomeController: UIViewController {
 
     
-    @IBOutlet weak var inputTextField: UITextField!
+    let inputTextField: UITextField = {
+        
+       let itf = UITextField()
+        
+        itf.borderStyle = .roundedRect
+        itf.placeholder = "Input"
+        itf.textAlignment = .center
+        itf.font = UIFont.systemFont(ofSize: 22)
+        itf.keyboardType = .numberPad
+        itf.translatesAutoresizingMaskIntoConstraints = false
+        
+        return itf
+    }()
+    
+    let goButton: UIButton = {
+       
+        let gb = UIButton()
+        gb.setTitle("Go", for: .normal)
+        gb.setTitleColor(.blue, for: .normal)
+        gb.translatesAutoresizingMaskIntoConstraints = false
+        
+        gb.addTarget(self, action: #selector(checkInputTextField), for: .touchUpInside)
+        
+        return gb
+    }()
     
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupViews()
     }
     
     
@@ -46,20 +71,47 @@ class HomeController: UIViewController {
     }
     
     
-    func createAlert(withErrorMessage errorMessage: String) {
+    func createAlert(withTitle title: String,withErrorMessage errorMessage: String) {
         
-        let alert = UIAlertController(title: "Error Code", message: errorMessage, preferredStyle: .alert)
+        let alert = UIAlertController(title: title, message: errorMessage, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         
         self.present(alert, animated: true)
     }
     
     
-    
-    @IBAction func goTapped(_ sender: Any) {
+    func setupViews() {
         
-        let inputInt = Int(inputTextField.text!)
-        createAlert(withErrorMessage: describeErrorMessage(forInput: inputInt!))
+        view.backgroundColor = .white
+        
+        view.addSubview(inputTextField)
+        view.addSubview(goButton)
+        
+        inputTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+        inputTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        inputTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+        inputTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        goButton.topAnchor.constraint(equalTo: inputTextField.bottomAnchor, constant: 80).isActive = true
+        goButton.leftAnchor.constraint(equalTo: inputTextField.leftAnchor).isActive = true
+        goButton.rightAnchor.constraint(equalTo: inputTextField.rightAnchor).isActive = true
+        goButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+    
+    
+    @objc func checkInputTextField() {
+        
+        if !inputTextField.text!.isEmpty {
+            
+            guard let inputText = inputTextField.text else { return }
+            let inputInt = Int(inputText)
+            
+            guard let safelyUnwrappedInputInt = inputInt else { return }
+            createAlert(withTitle: "Error Code Description", withErrorMessage: describeErrorMessage(forInput: safelyUnwrappedInputInt))
+        } else {
+            
+            createAlert(withTitle: "Empty TextField", withErrorMessage: "Please give a valid input")
+        }
     }
 }
 
